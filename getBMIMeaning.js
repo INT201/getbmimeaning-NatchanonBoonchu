52115759eabb5fbf0fe667675903dd1bbc4e7142
NatchanonBoonchu <natchanon.boonchu@mail.kmutt.ac.th>
2022-09-08 20:54:12 +0700

    Add code

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,19 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  let bmi = weight/height^2
+  return bmi ;
+
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  let bmi = calculateBMI(weight,height)
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi > 18.5 && bmi < 24.9) {
+    return "Normal weight";
+  } else if (bmi > 25.0){
+    return "Overweight";
+  }
 }
+console.log(getBMIMeaning(65, 1.8))
 module.exports = getBMIMeaning
